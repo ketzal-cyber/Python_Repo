049901a905c37aad754dd794744f9148ab4e5fb8
ketzal-cyber <hernandezmoreno.angel10@gmail.com>
2023-04-27 17:57:45 -0600

    Inclui comentarios sobre los metodos

--- 1000_Ficheros/EjercicioSeccion.py
+++ 1000_Ficheros/EjercicioSeccion.py
@@ -5,20 +5,24 @@ class Fichero:
     def __init__(self, nombre):
         self.nombre = nombre
 
+    #metodo para leer ficheros
     def leerFichero():
         fichero = open(self.nombre,"rt")
         texto = fichero.read()
         return texto
 
+    # metodo para guardar nuevo fichero
     def guardarFichero(self, texto):
         fichero = open(self.nombre, "wt")
         fichero.write(texto)
         fichero.close()
 
+    # metodo para agregar texto al fichero
     def agregarTexto(self, texto):
         fichero = open(self.nombre, "at")
         fichero.write(texto)
         fichero.close()
 
+    # metodo para eliminar un fichero
     def eliminarFichero(self, nombre):
         os.remove(self.nombre)
